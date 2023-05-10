93b8c02985e9aab2348400919138ed4897b38c80
abdelHamid.jinani <ahjinani@gmail.com>
2023-05-10 09:48:24 +0200

    fix hasRole admin

--- app/Session.php
+++ app/Session.php
@@ -42,7 +42,7 @@
         }
 
         public static function isAdmin(){
-            if(self::getUser() && self::getUser()->hasRole("ROLE_ADMIN")){
+            if(self::getUser() && self::getUser()->hasRole("admin")){
                 return true;
             }
             return false;
