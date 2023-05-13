8de18360faae8a8b3ea8a8257850142beaa100bb
AbdelHamid_JINANI <ahjinani>
2023-05-13 09:28:36 +0200

    translate addEvent form

--- view/security/addEvent.php
+++ view/security/addEvent.php
@@ -1,48 +1,48 @@
 <div id="box" name="box">
-    <h1 class="titleAddEvent">Add event</h1>
+    <h1 class="titleAddEvent">Ajouter un événement</h1>
 
     <form id="formRegister" method="POST" action="index.php?ctrl=event&action=addEvent">
 
-        <label for="titleEvent">Title event</label>
+        <label for="titleEvent">Titre :</label>
         <input type="text" name="titleEvent">
 
-        <label for="description">Description</label>
+        <label for="description">Description :</label>
         <input type="text" name="lastName">
 
-        <label for="zipcode">Zipcode</label>
+        <label for="zipcode">Code postal :</label>
         <input type="number" name="zipcode">
 
-        <label for="addres">Address</label>
+        <label for="addres">Adresse :</label>
         <input type="text" name="addres">
 
-        <label for="city">City</label>
+        <label for="city">Ville :</label>
         <input type="text" name="city">
 
-        <label for="country">Country</label>
+        <label for="country">Pays :</label>
         <input type="text" name="country">
 
-        <label for="dateStart">Date start</label>
+        <label for="dateStart">Date début :</label>
         <input type="date" name="dateStart">
 
-        <label for="dateEnd">Date end</label>
+        <label for="dateEnd">Date fin :</label>
         <input type="date" name="dateEnd">
 
-        <label for="maxUsers">Max users</label>
+        <label for="maxUsers">Max participants :</label>
         <input type="number" name="maxUsers">
 
-        <label for="contribution">Contribution</label>
+        <label for="contribution">Contribution :</label>
         <input type="number" name="contribution">
 
-        <label for="ImgEvent">Image event</label>
+        <label for="ImgEvent">Image :</label>
         <input type="text" name="ImgEvent">
 
-        <label for="alt">Alt</label>
+        <label for="alt">Alt :</label>
         <input type="text" name="alt">
 
-        <label for="user_id">user_id</label>
+        <label for="user_id">user_id :</label>
         <input type="number" name="user_id">
 
-        <label for="category">Category</label>
+        <label for="category">Categorie :</label>
         <select name="category">
             <option value="1">Culture</option>
             <option value="2">Sport</option>
