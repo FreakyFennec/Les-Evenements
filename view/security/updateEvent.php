80233aefd7ceb64973486d191d6006fc2642b464
abdel_Hamid.JINANI <ahjinani@gmail.com>
2023-05-15 14:04:51 +0200

    created form updateEvent

--- view/security/updateEvent.php
+++ view/security/updateEvent.php
@@ -1,15 +1,51 @@
 <div id="box" name="box">
-    <h1 class="titleformUpdateEvent">Modifier un événement</h1>
+    <h1 class="titleAddEvent">Modifier un événement</h1>
 
-    <form id="formUpdateEvent" method="POST" action="index.php?ctrl=security&action=updateEvent">
-        
-        <label for=""></label>
-        <input type="" name="" id="" required>
+    <form id="formRegister" method="POST" action="index.php?ctrl=event&action=updateEvent">
 
-        <label for=""></label>
-        <input type="" name="" id="" required>
+        <label for="titleEvent">Titre :</label>
+        <input type="text" id="titleEvent" name="titleEvent" required>
+
+        <label for="description">Description :</label>
+        <textarea name="description" id="description" cols="30" rows="10" required></textarea>
+
+        <label for="zipcode">Code postal :</label>
+        <input type="number" id="zipcode" name="zipcode" required>
+
+        <label for="address">Adresse :</label>
+        <input type="text" id="address" name="address" required>
+
+        <label for="city">Ville :</label>
+        <input type="text" id="city" name="city" required>
+
+        <label for="country">Pays :</label>
+        <input type="text" id="country" name="country" required>
+
+        <label for="dateStart">Date début :</label>
+        <input type="datetime-local" id="dateStart" name="dateStart" required>
+
+        <label for="dateEnd">Date fin :</label>
+        <input type="datetime-local" id="dateEnd" name="dateEnd" required>
+
+        <label for="maxUsers">Max participants :</label>
+        <input type="number" id="maxUsers" name="maxUsers" required>
+
+        <label for="contribution">Contribution :</label>
+        <input type="number" id="contribution" name="contribution" required>
+
+        <label for="ImgEvent">Image :</label>
+        <input type="text" id="ImgEvent" name="imgEvent" required>
+
+        <label for="alt">Alt :</label>
+        <input type="text" id="alt" name="alt" required>
+
+        <label for="category">Categorie :</label>
+        <select name="category" id="category" required>
+            <option value="1">Culture</option>
+            <option value="2">Sport</option>
+            <option value="3">Jeux de sociétés</option>
+        </select>
 
         <input id="submit" type="submit" name="submit" value="Envoyer">
-
     </form>
 </div>
