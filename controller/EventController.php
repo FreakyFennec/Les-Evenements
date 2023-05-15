4714996bbf7e8269c80967607d1941324657838c
abdel_Hamid.JINANI <ahjinani@gmail.com>
2023-05-15 16:44:46 +0200

    add update method

--- controller/EventController.php
+++ controller/EventController.php
@@ -73,8 +73,6 @@
                 $alt = filter_input(INPUT_POST, "alt", FILTER_SANITIZE_SPECIAL_CHARS);
                 $user_id = filter_input(INPUT_POST, "user_id", FILTER_SANITIZE_NUMBER_INT);
                 $category_id = filter_input(INPUT_POST, "category", FILTER_SANITIZE_NUMBER_INT);
-                // var_dump($_POST['submit']); 
-                // die();
     
                 // Verify there are filtered datas
                 if ($titleEvent &&
@@ -151,6 +149,71 @@
                 // And redirection to (view, method, id) with empty imput.
                 $this->redirectTo('event', 'detailEvent', $id);
             }
+        }
+
+        public function updateEvent(int $id)
+        {
+            $eventManager = new EventManager;
+
+            if (isset($_POST['sumbit'])) {
+
+                // Name of the columns placed in the super global $_POST that is stored in the variable.
+                $titleEvent = filter_input(INPUT_POST, "titleEvent", FILTER_SANITIZE_SPECIAL_CHARS);
+                $description = filter_input(INPUT_POST, "description", FILTER_SANITIZE_SPECIAL_CHARS);
+                $zipcode = filter_input(INPUT_POST, "zipcode", FILTER_SANITIZE_NUMBER_INT);
+                $address = filter_input(INPUT_POST, "address", FILTER_SANITIZE_SPECIAL_CHARS);
+                $city = filter_input(INPUT_POST, "city", FILTER_SANITIZE_SPECIAL_CHARS);
+                $country = filter_input(INPUT_POST, "country", FILTER_SANITIZE_SPECIAL_CHARS);
+                $dateStart = filter_input(INPUT_POST, "dateStart", FILTER_SANITIZE_SPECIAL_CHARS);
+                $dateEnd = filter_input(INPUT_POST, "dateEnd", FILTER_SANITIZE_SPECIAL_CHARS);
+                $maxUsers = filter_input(INPUT_POST, "maxUsers", FILTER_SANITIZE_NUMBER_INT);
+                $contribution = filter_input(INPUT_POST, "contribution", FILTER_SANITIZE_NUMBER_INT);
+                $imgEvent = filter_input(INPUT_POST, "imgEvent", FILTER_SANITIZE_SPECIAL_CHARS);
+                $alt = filter_input(INPUT_POST, "alt", FILTER_SANITIZE_SPECIAL_CHARS);
+                $user_id = filter_input(INPUT_POST, "user_id", FILTER_SANITIZE_NUMBER_INT);
+                $category_id = filter_input(INPUT_POST, "category", FILTER_SANITIZE_NUMBER_INT);
+
+                // Verify there are filtered datas
+                if ($titleEvent &&
+                    $description &&
+                    $zipcode &&
+                    $address &&
+                    $city &&
+                    $country &&
+                    $dateStart &&
+                    $dateEnd &&
+                    $maxUsers &&
+                    $contribution &&
+                    $imgEvent &&
+                    $alt &&
+                    $category_id) {
+                        
+                    // This associative array contain the columns name and de values.
+                    $data = [
+                        'titleEvent' => $titleEvent,
+                        'description' => $description,
+                        'zipcode' => $zipcode,
+                        'address' => $address,
+                        'city' => $city,
+                        'country' => $country,
+                        'dateStart' => $dateStart,
+                        'dateEnd' => $dateEnd,
+                        'maxUsers' => $maxUsers,
+                        'contribution' => $contribution,
+                        'imgEvent' => $imgEvent,
+                        'alt' => $alt,
+                        'user_id' => $user->getId(),
+                        'category_id' => $category_id,
+                    ];
+                    $eventManager->update($data);
+
+                    $this->redirectTo('event', 'updateEvent');
+                } // fin filter_input
+            } // fin submit
+            
+            return [
+                "view" => VIEW_DIR."security/updateEvent.php"
+            ];
         }
 
         public function removeUser() {
