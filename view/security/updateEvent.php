8ace20224972f0a7392c8f6fcbfcb30d411f2deb
AbdelHamid_JINANI <ahjinani>
2023-05-15 19:29:01 +0200

    ended form update event

--- view/security/updateEvent.php
+++ view/security/updateEvent.php
@@ -5,7 +5,7 @@
 <div id="box" name="box">
     <h1 class="titleAddEvent">Modifier un événement</h1>
 
-    <form id="formRegister" method="POST" action="index.php?ctrl=event&action=updateEvent">
+    <form id="formRegister" method="POST" action="index.php?ctrl=event&action=updateEvent&id=<?= $event->getId() ?>">
 
         <label for="titleEvent">Titre :</label>
         <input type="text" id="titleEvent" name="titleEvent" required value="<?= $event->getTitleEvent() ?>">
@@ -17,34 +17,34 @@
         <input type="number" id="zipcode" name="zipcode" required value="<?= $event->getZipcode() ?>"> 
 
         <label for="address">Adresse :</label>
-        <input type="text" id="address" name="address" required>
+        <input type="text" id="address" name="address" required  value="<?= $event->getAddress() ?>">
 
         <label for="city">Ville :</label>
         <input type="text" id="city" name="city" required value="<?= $event->getCity() ?>">
 
         <label for="country">Pays :</label>
-        <input type="text" id="country" name="country" required>
+        <input type="text" id="country" name="country" required value="<?= $event->getCountry() ?>">
 
         <label for="dateStart">Date début :</label>
         <input type="datetime-local" id="dateStart" name="dateStart" required value="<?= $event->getDateStartForm() ?>">
 
         <label for="dateEnd">Date fin :</label>
-        <input type="datetime-local" id="dateEnd" name="dateEnd" required>
+        <input type="datetime-local" id="dateEnd" name="dateEnd" required value="<?= $event->getDateEnd() ?>">
 
         <label for="maxUsers">Max participants :</label>
-        <input type="number" id="maxUsers" name="maxUsers" required>
+        <input type="number" id="maxUsers" name="maxUsers" required value="<?= $event->getMaxUsers() ?>">
 
         <label for="contribution">Contribution :</label>
-        <input type="number" id="contribution" name="contribution" required>
+        <input type="number" id="contribution" name="contribution" required value="<?= $event->getContribution() ?>">
 
         <label for="ImgEvent">Image :</label>
-        <input type="text" id="ImgEvent" name="imgEvent" required>
+        <input type="text" id="ImgEvent" name="imgEvent" required value="<?= $event->getImgEvent() ?>">
 
         <label for="alt">Alt :</label>
-        <input type="text" id="alt" name="alt" required>
+        <input type="text" id="alt" name="alt" required value="<?= $event->getAlt() ?>">
 
         <label for="category">Categorie :</label>
-        <select name="category" id="category" required>
+        <select name="category" id="category" required value="<?= $event->getCategory() ?>">
             <option value="1">Culture</option>
             <option value="2">Sport</option>
             <option value="3">Jeux de sociétés</option>
