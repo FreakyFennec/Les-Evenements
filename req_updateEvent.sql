fef4cfbf5d6edee7af36bd8c1850772c2e4b2642
abdel_Hamid.JINANI <ahjinani@gmail.com>
2023-05-15 16:09:19 +0200

    created req_updateEvent

---
+++ req_updateEvent.sql
@@ -0,0 +1,16 @@
+UPDATE event
+SET titleEvent = 'Le vélo-rail de Drulingen',
+	description = 'Voilà une activitée qui ravira les grands et les petits.',
+	zipcode = 67302,
+	address = '6 rue de Weyer',
+	city = 'Drulingen',
+	country = 'France',
+	dateStart = '2024-04-22 09:24:13',
+	dateEnd = '2024-02-29 10:24:37',
+	maxUsers = '50',
+	contribution = '45',
+	imgEvent = 'Velorail-Drulingen.jpg',
+	alt = 'Vélo-rail de Drulingen',
+	user_id = '7',
+	category_id = '2'
+WHERE id_event = 15;
