1e494f60b08f5fde9dacba87422b94d0c2702481
AbdelHamid_JINANI <ahjinani>
2023-05-19 18:31:42 +0200

    authorisation to comment or not

--- view/event/detailEvent.php
+++ view/event/detailEvent.php
@@ -2,9 +2,6 @@
 $detailEvent = $result["data"]['findOneById']; // It's un object array.
 $commentsEvent = $result["data"]['findCommentById'];
 $user = App\Session::getUser();
-echo '<pre>';
-var_dump($user);
-echo '</pre>';
 ?>
 
 <?php
@@ -16,35 +13,53 @@ echo '</pre>';
 
     <img src="public/img/<?= $detailEvent->getImgEvent(); ?>" alt="<?= $detailEvent->getAlt(); ?>" class="imgDetailEvent" id="imgDetailEvent">
 
-    <div class="descAndInfos">
-        <!-- If not connected -->
-        <div class="infoForAll">
-            <div class="dateStartEnd">
-                <p class="dateEvent">Du : <?= $detailEvent->getDateStart(); ?><br /> au : <?= $detailEvent->getDateEnd(); ?></p>
+    
+        <!-- If connected as admin or moderator -->       
+        <?php
+        if ($user && ($user->getStatus() == 'admin' || $user->getStatus() == 'moderator')) {
+        ?>
+        <div class="descAndInfos">
+            <div class="infoForAll">
+                <div class="dateStartEnd">
+                    <p class="dateEvent">Du : <?= $detailEvent->getDateStart(); ?><br /> au : <?= $detailEvent->getDateEnd(); ?></p>
+                </div>
+                
+                <p class="address">Au <?= $detailEvent->getAddress(); ?></p>
+                <p class="city"><?= $detailEvent->getCity(); ?></p>
+                <p class="zipcode"><?= $detailEvent->getZipcode(); ?></p>
+                <p class="maxUsers">Participants max. : <?= $detailEvent->getMaxUsers(); ?> pers.</p>
+                <p class="descSmall"><?= $detailEvent->getDescription(); ?></p>
             </div>
-            
-            <p class="address">Au <?= $detailEvent->getAddress(); ?></p>
-            <p class="city"><?= $detailEvent->getCity(); ?></p>
-            <p class="zipcode"><?= $detailEvent->getZipcode(); ?></p>
-            <p class="maxUsers">Participants max. : <?= $detailEvent->getMaxUsers(); ?> pers.</p>
-            <p class="descSmall"><?= $detailEvent->getDescription(); ?></p>
-        </div>
-        
-        <!-- If connected as admin or moderator -->
-        <?php
-        if (isset($user) && $user->getStatus() == 'admin' || $user->getStatus() == 'moderator') {
-        ?>
+
             <button class="modifEvent" onclick="window.location.href = 'index.php?ctrl=event&action=updateEvent&id=<?= $detailEvent->getId() ?>';">Modifier</button>
-        <?php
-        } 
-        ?>
         
+
+        <!-- If not connected -->       
+        <?php
+        } else {
+        ?>
+        <div class="descAndInfos">
+            <div class="infoForAll">
+                <div class="dateStartEnd">
+                    <p class="dateEvent">Du : <?= $detailEvent->getDateStart(); ?><br /> au : <?= $detailEvent->getDateEnd(); ?></p>
+                </div>
+                
+                <p class="address">Au <?= $detailEvent->getAddress(); ?></p>
+                <p class="city"><?= $detailEvent->getCity(); ?></p>
+                <p class="zipcode"><?= $detailEvent->getZipcode(); ?></p>
+                <p class="maxUsers">Participants max. : <?= $detailEvent->getMaxUsers(); ?> pers.</p>
+                <p class="descSmall"><?= $detailEvent->getDescription(); ?></p>
+            </div>
+        <?php 
+        }
+        ?>       
     </div>
         
     <div id="map">
         <!-- Map's place -->
     </div>
 </div>
+
 <div class="comArea">
     <h2 class="titleComArea">Commentaires</h2>
 
@@ -77,21 +92,26 @@ echo '</pre>';
     } ?>
 </div>
 
-<div id="box" name="box">
-    <h2 class="titleAddComArea">Ajoutez votre commentaire</h2>
+<?php
+if ($user && ($user->getStatus() == 'admin' || $user->getStatus() == 'moderator' || $user->getStatus() == 'user')) {
+?>
+    <div id="box" name="box">
+        <h2 class="titleAddComArea">Ajoutez votre commentaire</h2>
 
-    <form id="formRegister" method="POST" action="index.php?ctrl=event&action=addComment&id=<?= $detailEvent->getId(); ?>&user_id=">
+        <form id="formRegister" method="POST" action="index.php?ctrl=event&action=addComment&id=<?= $detailEvent->getId(); ?>&user_id=">
 
-        <label for="titleComment">Titre du commentaire</label>
-        <input type="text" name="titleComment">
+            <label for="titleComment">Titre du commentaire</label>
+            <input type="text" name="titleComment">
 
-        <label for="comment">Votre commentaire</label>
-        <input type="text" name="comment">
+            <label for="comment">Votre commentaire</label>
+            <input type="text" name="comment">
 
-        <input id="submit" type="submit" name="submit" value="Submit">
-    </form>
-</div>
-
+            <input id="submit" type="submit" name="submit" value="Submit">
+        </form>
+    </div>
+<?php
+}
+?>
 
     
 
