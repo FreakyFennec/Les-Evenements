2be9e607f528a8e5c18eff746d7dd2388f21927e
AbdelHamid_JINANI <ahjinani>
2023-05-22 15:06:43 +0200

    add content rules.php

--- view/rules.php
+++ view/rules.php
@@ -1,1 +1,44 @@
-<h1>Réglement du site</h1>
+<section class="secRules">
+    <h1>Réglement du site</h1>
+
+    <article>
+        <p class="titleArticle">1. Introduction</p>
+        <p class="dataArticle">a. Objet : Ce règlement a pour but de définir les conditions d'utilisation du site web [nom du site web].</p>
+        <p class="dataArticle">b. Acceptation : L'utilisation du site web implique l'acceptation sans réserve de ce règlement.</p>
+    </article>
+    <article>
+        <p class="titleArticle">2. Propriété intellectuelle</p>
+        <p class="dataArticle">a. Droits d'auteur : Tout le contenu présent sur le site web (textes, images, vidéos, etc.) est protégé par des droits d'auteur.</p>
+        <p class="dataArticle">b. Utilisation autorisée : Les utilisateurs peuvent consulter le contenu à titre personnel, mais toute reproduction ou utilisation commerciale est interdite sans autorisation préalable.</p>
+    </article>
+    <article>
+        <p class="titleArticle">3. Responsabilités des utilisateurs</p>
+        <p class="dataArticle">a. Comportement : Les utilisateurs doivent adopter un comportement respectueux envers les autres utilisateurs et le site web lui-même.</p>
+        <p class="dataArticle">b. Contenu généré par les utilisateurs : Les utilisateurs sont responsables du contenu qu'ils publient sur le site web et s'engagent à respecter les lois en vigueur.</p>
+        <p class="dataArticle">c. Utilisation licite : Il est interdit d'utiliser le site web à des fins illégales, diffamatoires, frauduleuses ou nuisibles.</p>
+    </article>
+    <article>
+        <p class="titleArticle">4. Protection des données personnelles</p>
+        <p class="dataArticle">a. Collecte des données : Le site web peut collecter des données personnelles conformément à sa politique de confidentialité.</p>
+        <p class="dataArticle">b. Utilisation des données : Les données personnelles collectées seront utilisées uniquement dans le cadre prévu par la politique de confidentialité.</p>
+    </article>
+    <article>
+        <p class="titleArticle">5. Cookies</p>
+        <p class="dataArticle">a. Utilisation des cookies : Le site web peut utiliser des cookies pour améliorer l'expérience de navigation des utilisateurs. Les utilisateurs peuvent accepter ou refuser les cookies.</p>
+    </article>
+    <article>
+        <p class="titleArticle">6. Limitation de responsabilité</p>
+        <p class="dataArticle">a. Disponibilité : Le site web ne garantit pas une disponibilité continue et peut être temporairement inaccessible pour des raisons techniques.</p>
+        <p class="dataArticle">b. Contenu externe : Le site web peut contenir des liens vers des sites externes sur lesquels il n'a aucun contrôle. Il n'assume aucune responsabilité quant au contenu de ces sites.</p>
+    </article>
+    <article>
+        <p class="titleArticle">7. Modifications et résiliation</p>
+        <p class="dataArticle">a. Modifications du règlement : Le site web se réserve le droit de modifier ce règlement à tout moment. Les utilisateurs seront informés de ces modifications.</p>
+        <p class="dataArticle">b. Résiliation : Le site web se réserve le droit de résilier l'accès des utilisateurs qui ne respectent pas ce règlement.</p>
+    </article>
+    <article>
+        <p class="titleArticle">8. Loi applicable et juridiction compétente</p>
+        <p class="dataArticle">a. Loi applicable : Ce règlement est régi par [loi applicable].</p>
+        <p class="dataArticle">b. Juridiction compétente : Tout litige relatif à ce règlement relève de la compétence des tribunaux de [lieu de juridiction].</p>
+    </article>
+</section>
