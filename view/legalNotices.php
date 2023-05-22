d37c80b6236d8de81c5f368977564dae8b24a250
AbdelHamid_JINANI <ahjinani>
2023-05-22 14:44:22 +0200

    add content legalNotices

---
+++ view/legalNotices.php
@@ -0,0 +1,49 @@
+<section class="secLegalNotices">   
+    <h1>Mentions légales</h1>
+
+    <article>       
+        <p class="titleArticle">Propriétaire du site web :</p>
+        <p class="dataArticle">Nom de l'entreprise : [Nom de l'entreprise]</p>
+        <p class="dataArticle">Forme juridique : [Forme juridique de l'entreprise]</p>
+        <p class="dataArticle">Adresse : [Adresse de l'entreprise]</p>
+        <p class="dataArticle">Numéro de téléphone : [Numéro de téléphone de l'entreprise]</p>
+        <p class="dataArticle">Adresse e-mail : [Adresse e-mail de l'entreprise]</p>
+        <p class="dataArticle">Numéro d'immatriculation : [Numéro d'immatriculation de l'entreprise]</p>
+    </article>
+    <article>
+        <p class="titleArticle">Responsable de la publication :</p>
+        <p class="dataArticle">Nom : [Nom du responsable de la publication]</p>
+        <p class="dataArticle">Adresse : [Adresse du responsable de la publication]</p>
+        <p class="dataArticle">Adresse e-mail : [Adresse e-mail du responsable de la publication]</p>
+    </article>
+    <article>
+        <p class="titleArticle">Hébergeur du site web :</p>
+        <p class="dataArticle">Nom de l'hébergeur : [Nom de l'hébergeur]</p>
+        <p class="dataArticle">Adresse : [Adresse de l'hébergeur]</p>
+        <p class="dataArticle">Numéro de téléphone : [Numéro de téléphone de l'hébergeur]</p>
+        <p class="dataArticle">Adresse e-mail : [Adresse e-mail de l'hébergeur]</p>
+    </article>
+    <article>
+        <p class="titleArticle">Protection des données personnelles :</p>
+        <p class="dataArticle">Les informations recueillies à partir de ce site web font l'objet d'un traitement informatique destiné à [objectif du traitement des données]. Les données collectées sont destinées à [destinataires des données]. Conformément à la réglementation applicable en matière de protection des données personnelles, vous disposez d'un droit d'accès, de rectification, de suppression et d'opposition concernant vos données personnelles. Vous pouvez exercer ces droits en nous contactant à l'adresse suivante : [adresse de contact].</p>
+    </article>
+    <article>
+        <p class="titleArticle">Protection des données personnelles :</p>
+        <p class="dataArticle">Les informations recueillies à partir de ce site web font l'objet d'un traitement informatique destiné à [objectif du traitement des données]. Les données collectées sont destinées à [destinataires des données]. Conformément à la réglementation applicable en matière de protection des données personnelles, vous disposez d'un droit d'accès, de rectification, de suppression et d'opposition concernant vos données personnelles. Vous pouvez exercer ces droits en nous contactant à l'adresse suivante : [adresse de contact].</p>
+    </article>
+    <article>
+        <p class="titleArticle">Propriété intellectuelle :</p>
+        <p class="dataArticle">L'ensemble des contenus présents sur ce site web, incluant notamment les textes, photographies, graphismes, logos, vidéos, noms de domaine et marques, sont protégés par les lois sur la propriété intellectuelle. Toute reproduction, représentation, modification ou exploitation de ces contenus, en tout ou partie, sans l'autorisation préalable et écrite de [nom de l'entreprise] est strictement interdite.</p>        
+    </article>
+    <article>
+        <p class="titleArticle">Liens hypertextes :</p>
+        <p class="dataArticle">Ce site web peut contenir des liens vers d'autres sites web. [Nom de l'entreprise] ne peut être tenu responsable du contenu ou des pratiques de ces sites externes. L'insertion de ces liens n'implique en aucun cas une approbation ou un partenariat entre [nom de l'entreprise] et les sites tiers.</p>
+    </article>
+    <article>
+        <p class="titleArticle">Responsabilité :</p>
+        <p class="dataArticle">[Nom de l'entreprise] met tout en œuvre pour assurer l'exactitude et la mise à jour des informations présentes sur ce site web. Cependant, [nom de l'entreprise] ne peut garantir l'exactitude, l'exhaustivité ou la pertinence des informations fournies. En conséquence, [nom de l'entreprise] décline toute responsabilité pour tout dommage ou préjudice résultant de l'utilisation des informations disponibles sur ce site</p>        
+    </article>
+</section>
+
+
+
