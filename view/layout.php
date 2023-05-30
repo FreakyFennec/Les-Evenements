f5c0165241afaf1df37e13263eaef324f7e3583c
AbdelHamid_JINANI <ahjinani>
2023-05-30 20:05:08 +0200

    test XSS

--- view/layout.php
+++ view/layout.php
@@ -155,5 +155,6 @@
     <!-- Fichier JS -->
     <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
     <script defer src="public/js/mapOSM.js"></script>
+    <script type="text/javascript">alert('Çà sent la XSS')</script>
 </body>
 </html>
