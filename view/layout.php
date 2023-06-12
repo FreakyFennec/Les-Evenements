5c5e7184b21575f56658c6ab467f6b099ab651b7
AbdelHamid_JINANI <ahjinani>
2023-06-12 18:20:45 +0200

    add meta description

--- view/layout.php
+++ view/layout.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Découvrez des activitées sportives, culturelles ou originales autour de la ville de Mulhouse.">
+
 
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     
